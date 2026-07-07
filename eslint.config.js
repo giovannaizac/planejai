f002c968d2f34d406371e2362e0433b3f83206ed
Giovanna Izac <giovannaizac2@hotmail.com>
2026-07-06 21:54:09 -0300

    feat/Adiciona configuarações do Button

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,79 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import js from '@eslint/js'
-import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
+import globals from 'globals'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
+	globalIgnores(['dist', 'node_modules']),
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+			eslintConfigPrettier,
+		],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: globals.browser,
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: __dirname,
+			},
+		},
+		plugins: {
+			import: importPlugin,
+			prettier: prettierPlugin,
+			'simple-import-sort': simpleImportSort,
+			'unused-imports': unusedImports,
+		},
+		settings: {
+			'import/resolver': {
+				typescript: {
+					project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
+					alwaysTryTypes: true,
+				},
+			},
+		},
+		rules: {
+			'prettier/prettier': [
+				'error',
+				{
+					singleQuote: true,
+					semi: false,
+					trailingComma: 'all',
+					tabWidth: 2,
+					useTabs: true,
+					printWidth: 100,
+				},
+			],
+			'unused-imports/no-unused-imports': 'error',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePattern: '^_',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+				},
+			],
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error',
+			'import/order': 'off',
+		},
+	},
 ])
