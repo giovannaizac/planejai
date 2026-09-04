64596d9c2628dc43183ae99d418d83692e15a00a
Giovanna Izac <giovannaizac2@hotmail.com>
2026-09-04 15:12:44 -0300

    Add basename to router configuration
    Refactor router configuration to include basename.

--- src/router.tsx
+++ src/router.tsx
@@ -1,30 +1,30 @@
 import { createBrowserRouter } from 'react-router-dom'
-
 import { RootLayout } from './components/layout/RootLayout'
 import { SimulationFormPage } from './pages/SimulationFormPage'
 import { SimulationHistoryPage } from './pages/SimulationHistoryPage'
 import { SimulationResultsPage } from './pages/SimulationResultsPage'
 
-export const router = createBrowserRouter([
-	{
-		element: <RootLayout />,
-		children: [
-			{
-				path: '/',
-				element: <SimulationFormPage />,
-			},
-			{
-				path: '/resultado/:id',
-				element: <SimulationResultsPage />,
-			},
-			{
-				path: '/historico',
-				element: <SimulationHistoryPage />,
-			},
-		],
-	},
-]
-										  {
+export const router = createBrowserRouter(
+  [
+    {
+      element: <RootLayout />,
+      children: [
+        {
+          path: '/',
+          element: <SimulationFormPage />,
+        },
+        {
+          path: '/resultado/:id',
+          element: <SimulationResultsPage />,
+        },
+        {
+          path: '/historico',
+          element: <SimulationHistoryPage />,
+        },
+      ],
+    },
+  ],
+  {
     basename: '/planejai',
   }
-										 )
+)
